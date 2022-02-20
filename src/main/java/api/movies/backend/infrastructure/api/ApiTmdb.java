c5d7e5d578feffa0fb3b9332544e07b1ec4b0e81
Ferran Juanals <ferranjuanals@gmail.com>
2022-02-20 18:46:32 +0100

    Imported MovieDto to ApiTmdb

--- src/main/java/api/movies/backend/infrastructure/api/ApiTmdb.java
+++ src/main/java/api/movies/backend/infrastructure/api/ApiTmdb.java
@@ -1,5 +1,6 @@
 package api.movies.backend.infrastructure.api;
 
+import api.movies.backend.application.dto.MovieDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
